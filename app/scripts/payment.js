74ac31fd57bbb6f7c147543e2e9c216bb935613a
余泽江 <yzj1995@vip.qq.com>
2014-08-27 16:25:55 +0800

    完成付款页

---
+++ app/scripts/payment.js
@@ -0,0 +1,59 @@
+function refresh () {
+    var boughtList = Order.all();
+    $('.present-item').remove();
+    _(boughtList).each(function (item) {
+        if(item.free() > 0) {
+            var presentItem = $('<tr class="present-item">\
+                                    <td>' + item.type + '</td>\
+                                    <td class="item-name">' + item.name + '</td>\
+                                    <td class="item-free">' + item.free() + '</td>\
+                                </tr>');
+            $('#present-table').append(presentItem);
+        }
+    });
+    $('#total').text(Order.totalPrice(boughtList).toFixed(2) + '元');
+    $('#save').text(Order.savePrice(boughtList).toFixed(2) + '元');
+    $('#count').text(cartCount());
+}
+
+$(document).ready(function () {
+    var boughtList = Order.all();
+    Order.getPromotion(boughtList, loadPromotions());
+
+    _(boughtList).each(function (item) {
+        var boughtItem = $('<tr class="bought-item">\
+                                <td>' + item.type + '</td>\
+                                <td class="item-name">' + item.name + '</td>\
+                                <td class="item-price">' + item.price + '</td>\
+                                <td>' + item.unit + '</td>\
+                                <td>' + item.count + '</td>\
+                                <td class="item-sum">' + item.sumDisplay() + '</td>\
+                            </tr>');
+        $('#bought-table').append(boughtItem);
+    }, this);
+
+    refresh();
+
+    $('.item-add').on('click', function () {
+        var itemName = $(this).closest('.bought-item').find('.item-name').text();
+        var item = Order.findByName(itemName);
+        item.addCount();
+        var itemCount = $(this).closest('div').find('.item-count');
+        itemCount.text(item.count);
+        var itemSum = $(this).closest('.bought-item').find('.item-sum');
+        itemSum.text(item.sumDisplay());
+        refresh();
+    });
+
+    $('.item-minus').on('click', function () {
+        var itemName = $(this).closest('.bought-item').find('.item-name').text();
+        var item = Order.findByName(itemName);
+        item.minusCount();
+        var itemCount = $(this).closest('div').find('.item-count');
+        itemCount.text(item.count);
+        var itemSum = $(this).closest('.bought-item').find('.item-sum');
+        itemSum.text(item.sumDisplay());
+        refresh();
+    });
+
+});
