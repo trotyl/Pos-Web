570e8145ac3ec3eb2110f5bd24b67a2f86c78f37
余泽江 <yzj1995@vip.qq.com>
2014-08-27 10:30:34 +0800

    完成商品列表页主体

--- app/scripts/items.js
+++ app/scripts/items.js
@@ -1,3 +1,12 @@
-/**
- * Created by yuzejiang on 14/8/27.
- */
+$(document).ready(function () {
+    $('.list-item').on('click', 'button', function () {
+        var count = $('#count');
+        count.text(+count.text() + 1);
+
+        var boughtItems = JSON.parse(localStorage.getItem('boughtItems')) || {};
+        var itemName = $(this).closest('.list-item').find('.item-name').text();
+        boughtItems[itemName] || (boughtItems[itemName] = 0);
+        boughtItems[itemName] += 1;
+        localStorage.boughtItems = JSON.stringify(boughtItems);
+    });
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+function cartCount () {
+    var boughtItems = JSON.parse(localStorage.getItem('boughtItems')) || {};
+    var total = 0;
+    for(var item in boughtItems) {
+        total += boughtItems[item] || 0;
+    }
+    return total;
+}
+
+$(document).ready(function () {
+    $('#count').text(cartCount());
+});
