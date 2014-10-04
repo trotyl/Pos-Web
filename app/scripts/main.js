eba8602a21ad02b5ad5ff8b0fc2b7a3a6a292267
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-04 22:48:35 +0800

    重构实现逻辑 完成首页

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,21 @@
-function cartCount () {
-    var boughtItems = JSON.parse(localStorage.boughtItems);
-    var total = 0;
-    for(var item in boughtItems) {
-        total += boughtItems[item].count || 0;
-    }
-    return total;
+function viewPath (view) {
+    return 'views/' + view + '.html';
+}
+
+function loadView (view) {
+    $.get(viewPath(view), function (data) {
+        $('#view').html(data);
+    });
+}
+
+function loadCartCount () {
+    var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    return _(cartItems).reduce(function (sum, item) {
+        return sum + item.count();
+    }, 0)
 }
 
 $(document).ready(function () {
-    localStorage.getItem('boughtItems') || (localStorage.boughtItems = JSON.stringify({}));
-    $('#count').text(cartCount());
+    loadCartCount();
+    loadView('home');
 });
