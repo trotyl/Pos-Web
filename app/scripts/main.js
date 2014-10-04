25c1c838cbb67f322692cdb281f40f0d0b2326b5
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-04 23:40:12 +0800

    实现商品列表页加载数据

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,19 @@
-function viewPath (view) {
-    return 'views/' + view + '.html';
+function buyButtonFormer () {
+    return '<button class="btn btn-primary btm-sm">加入购物车</button>';
 }
 
-function loadView (view) {
-    $('#view').data('view', view);
-    $.get(viewPath(view), function (data) {
-        $('#view').html(data);
-        listenerInitiate();
-        highlightInitiate();
-    });
+function cartAddItem (itemName) {
+    var cartItems = JSON.parse(localStorage.getItem('cartItems')) || {};
+    cartItems[itemName] || (cartItems[itemName] = _(loadAllItems()).find({name: itemName}));
+    cartItems[itemName].count += 1;
+    localStorage.boughtItems = JSON.stringify(cartItems);
 }
 
-function loadCartCount () {
+function cartCountInitiate () {
     var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
     return _(cartItems).reduce(function (sum, item) {
         return sum + item.count();
     }, 0)
-}
-
-function listenerInitiate () {
-    $('.view-option').on('click', function () {
-        var view = $(this).data('view');
-        loadView(view);
-    })
 }
 
 function highlightInitiate () {
@@ -35,9 +26,84 @@ function highlightInitiate () {
     })
 }
 
-$(document).ready(function () {
-    loadCartCount();
-    loadView('home');
+function listenerInitiate () {
+    $('.view-option').on('click', function () {
+        var view = $(this).data('view');
+        loadView(view);
+    });
+}
+
+function listItemFormer (item) {
+    var itemLine = '<tr class="list-item"><td class="item-type"></td><td class="item-name"></td><td class="item-price"></td><td class="item-unit"></td><td class="item-buy"></td></tr>';
+    var itemDom = $(itemLine);
+    itemDom.data('item', item.name);
+    itemDom.find('.item-type').text(item.type);
+    itemDom.find('.item-name').text(item.name);
+    itemDom.find('.item-price').text(item.price);
+    itemDom.find('.item-unit').text(item.unit);
+    itemDom.find('.item-buy').html(buyButtonFormer());
+    return itemDom;
+}
+
+function listListenerInitiate () {
+    $('.list-item').on('click', 'button', function () {
+        cartAddItem($(this).closest('.list-item').data('itemName'));
+    });
+}
+
+function listViewInitiate () {
+    var items = loadAllItems();
+    _(items).each(function (item) {
+        var listItem = listItemFormer(item);
+        $('#item-table').append(listItem);
+    });
+    listListenerInitiate();
+}
+
+function loadAllItems() {
+    return [
+        new Item('ITEM000000', '可口可乐', '瓶', 3.00, '饮料'),
+        new Item('ITEM000001', '雪碧', '瓶', 3.00, '饮料'),
+        new Item('ITEM000002', '苹果', '斤', 5.50, '水果'),
+        new Item('ITEM000003', '荔枝', '斤', 15.00, '水果'),
+        new Item('ITEM000004', '电池', '个', 2.00, '生活用品'),
+        new Item('ITEM000005', '方便面', '袋', 4.50, '食品')
+    ];
+}
+
+function loadPromotions() {
+    return [
+        new Promotion('BUY_TWO_GET_ONE_FREE', [
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000005'
+        ])
+    ]
+}
+
+function loadView (view) {
+    $('#view').data('view', view);
+    $.get(viewPath(view), function (data) {
+        $('#view').html(data);
+        refresh();
+    });
+}
+
+function refresh () {
+    cartCountInitiate();
     listenerInitiate();
     highlightInitiate();
+    if($('#view').data('view') == 'list') {
+        listViewInitiate();
+    }
+
+}
+
+function viewPath (view) {
+    return 'views/' + view + '.html';
+}
+
+$(document).ready(function () {
+    loadView('home');
+    refresh();
 });
