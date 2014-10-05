342333d0730eeb4d6431e8eea718208b137cf629
Trotyl Yu <yzj1995@vip.qq.com>
2014-10-05 19:09:35 +0800

    重命名函数

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-function boutghtItemFormer (item) {
+function boutghtItemHelper (item) {
     var itemLine = '<tr class="bought-item"><td class="item-type"></td><td class="item-name"></td><td class="item-price"></td><td class="item-unit"></td><td class="item-count"></td><td class="item-sum"></td></tr>';
     var itemDom = $(itemLine);
     itemDom.find('.item-type').text(item.type);
@@ -6,11 +6,11 @@ function boutghtItemFormer (item) {
     itemDom.find('.item-price').text(item.price);
     itemDom.find('.item-unit').text(item.unit);
     itemDom.find('.item-count').text(item.count);
-    itemDom.find('.item-sum').text(sumPriceFormer(item));
+    itemDom.find('.item-sum').text(sumPriceHelper(item));
     return itemDom;
 }
 
-function buyButtonFormer () {
+function buyButtonHelper () {
     return '<button class="btn btn-primary btm-sm">加入购物车</button>';
 }
 
@@ -19,19 +19,19 @@ function cartCountInitiate () {
     $('#cart-count').text(count);
 }
 
-function cartItemFormer (item) {
+function cartItemHelper (item) {
     var itemLine = '<tr class="cart-item"><td class="item-type"></td><td class="item-name"></td><td class="item-price"></td><td class="item-unit"></td><td class="item-count"></td><td class="item-sum"></td></tr>';
     var itemDom = $(itemLine);
     itemDom.find('.item-type').text(item.type);
     itemDom.find('.item-name').text(item.name);
     itemDom.find('.item-price').text(item.price);
     itemDom.find('.item-unit').text(item.unit);
-    itemDom.find('.item-count').html(countButtonGroupFormer(item.count));
-    itemDom.find('.item-sum').text(sumPriceFormer(item));
+    itemDom.find('.item-count').html(countButtonGroupHelper(item.count));
+    itemDom.find('.item-sum').text(sumPriceHelper(item));
     return itemDom;
 }
 
-function cartListenerInitiate () {
+function cartListener () {
     $('.item-count').on('click', 'button', function () {
         var itemName = $(this).closest('.cart-item').find('.item-name').text();
         var item = Order.findByName(itemName);
@@ -42,7 +42,7 @@ function cartListenerInitiate () {
             item.minusCount();
         }
         $(this).closest('.btn-group').find('.number').text(item.count);
-        $(this).closest('.cart-item').find('.item-sum').text(sumPriceFormer(item));
+        $(this).closest('.cart-item').find('.item-sum').text(sumPriceHelper(item));
         cartCountInitiate();
         if($('#cart-count').text() == 0) {
             loadView('list');
@@ -50,19 +50,19 @@ function cartListenerInitiate () {
     })
 }
 
-function cartViewInitiate () {
+function cartController () {
     Order.getPromotion(loadPromotions());
     var cartItems = Order.all();
     _(cartItems).each(function (item) {
-        var cartItem = cartItemFormer(item);
+        var cartItem = cartItemHelper(item);
         $('#cart-table').append(cartItem);
     });
     $('#cart-fare').text(Order.fare().toFixed(2));
     $('#cart-saving').text(Order.saving().toFixed(2));
-    cartListenerInitiate();
+    cartListener();
 }
 
-function countButtonGroupFormer (count) {
+function countButtonGroupHelper (count) {
     return '<div class="btn-group">\
                 <button class="btn btn-default" data-operation="minus">-</button>\
                 <button class="btn btn-default number" disabled="disabled">' + count + '</button>\
@@ -70,7 +70,7 @@ function countButtonGroupFormer (count) {
             </div>'
 }
 
-function freeItemFormer (item) {
+function freeItemHelper (item) {
     var itemLine = '<tr class="free-item"><td class="item-type"></td><td class="item-name"></td><td class="item-free"></td></tr>';
     var itemDom = $(itemLine);
     itemDom.find('.item-type').text(item.type);
@@ -89,29 +89,22 @@ function highlightInitiate () {
     })
 }
 
-function homeViewInitiate () {
+function homeController () {
 
 }
 
-function listenerInitiate () {
-    $('.view-option').on('click', function () {
-        var view = $(this).data('view');
-        loadView(view);
-    });
-}
-
-function listItemFormer (item) {
+function listItemHelper (item) {
     var itemLine = '<tr class="list-item"><td class="item-type"></td><td class="item-name"></td><td class="item-price"></td><td class="item-unit"></td><td class="item-buy"></td></tr>';
     var itemDom = $(itemLine);
     itemDom.find('.item-type').text(item.type);
     itemDom.find('.item-name').text(item.name);
     itemDom.find('.item-price').text(item.price);
     itemDom.find('.item-unit').text(item.unit);
-    itemDom.find('.item-buy').html(buyButtonFormer());
+    itemDom.find('.item-buy').html(buyButtonHelper());
     return itemDom;
 }
 
-function listListenerInitiate () {
+function listListener () {
     $('.list-item').on('click', 'button', function () {
         var itemName = $(this).closest('.list-item').find('.item-name').text();
         var item = Order.findByName(itemName);
@@ -120,71 +113,79 @@ function listListenerInitiate () {
     });
 }
 
-function listViewInitiate () {
+function listController () {
     var items = loadAllItems();
     _(items).each(function (item) {
-        var listItem = listItemFormer(item);
+        var listItem = listItemHelper(item);
         $('#item-table').append(listItem);
     });
-    listListenerInitiate();
+    listListener();
 }
 
 function loadView (view) {
-    $('#view').data('view', view).load(viewPath(view), function () {
-        refresh();
+    $('#view').data('view', view).load(viewHelper(view), function () {
+        mainController();
     });
 }
 
-function paymentListenerInitiate () {
+function mainListener () {
+    $('.view-option').on('click', function () {
+        var view = $(this).data('view');
+        loadView(view);
+    });
+}
+
+
+function paymentListener () {
     $('#confirm').on('click', function () {
         Order.clear();
         loadView('home');
     })
 }
 
-function paymentViewInitiate () {
+function paymentController () {
     var cartItems = Order.all();
     _(cartItems).each(function (item) {
-        var boughtItem = boutghtItemFormer(item);
+        var boughtItem = boutghtItemHelper(item);
         $('#list-table').append(boughtItem);
     });
     var freeItems = _(cartItems).filter(function (item) {
         return item.free() > 0;
     });
     _(freeItems).each(function (item) {
-        var freeItem = freeItemFormer(item);
+        var freeItem = freeItemHelper(item);
         $('#free-table').append(freeItem);
     });
     $('#dateTime').text(moment().format('YYYY年MM月DD日 HH:mm:ss'));
     $('#cart-fare').text(Order.fare().toFixed(2));
     $('#cart-saving').text(Order.saving().toFixed(2));
-    paymentListenerInitiate();
+    paymentListener();
 }
 
-function refresh () {
+function mainController () {
     cartCountInitiate();
-    listenerInitiate();
+    mainListener();
     highlightInitiate();
     var view = $('#view').data('view');
     var doIt = {
-        home: homeViewInitiate,
-        list: listViewInitiate,
-        cart: cartViewInitiate,
-        payment: paymentViewInitiate
+        home: homeController,
+        list: listController,
+        cart: cartController,
+        payment: paymentController
     };
     doIt[view]();
 }
 
-function sumPriceFormer (item) {
+function sumPriceHelper (item) {
     var extra = item.free() > 0? ' (原价：' + item.total() + '元)': '';
     return item.fare() + '元' + extra;
 }
 
-function viewPath (view) {
+function viewHelper (view) {
     return 'views/' + view + '.html';
 }
 
 $(document).ready(function () {
     loadView('home');
-    refresh();
+    mainController();
 });
